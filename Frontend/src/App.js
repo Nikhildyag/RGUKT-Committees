cd76dee918344272d69810e730c19cc6cab9939c
RathlavathRamesh <rameshrguktbasar@gmail.com>
2024-07-08 12:58:57 +0530

    sample commities added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,14 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import './App.css'
 import Homepage from './components/Home'
-import Header from './components/Header'
+import Commity1 from './components/Commity1'
+// import DesktopCommities from './components/DesktopCommities'
 
 const App = () => (
   <BrowserRouter>
   <Header/>
     <Routes>
-      <Header />
       <Route exact path="/" Component={Homepage} />
+      <Route exact path="/commtiy1" Component={Commity1}/>
     </Routes>
   </BrowserRouter>
 )
---
+++ Frontend/src/components/Commity1/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import DesktopCommities from '../DesktopCommities'
+
+const Commity1 = () => {
+  return (
+    <div className='flex'>
+     <DesktopCommities/>
+    <div className='bg-lime-200 h-44 text-black'>
+       <h1 className='text-black block'>welcome to Comity1</h1>
+    </div>
+    </div>
+   
+  )
+}
+export default Commity1
