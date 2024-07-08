d829b227a3465cf5d0d1077fcbe85bf2de1efb50
RathlavathRamesh <rameshrguktbasar@gmail.com>
2024-07-08 18:50:02 +0530

    commitie names added

--- Frontend/src/components/DesktopCommities/index.js
+++ Frontend/src/components/DesktopCommities/index.js
@@ -3,87 +3,110 @@ import { Link } from 'react-router-dom'
 
 const DesktopCommities = () => {
   return (
-    <div className='min-h-screen h-full sm:hidden md:block lg:block xl:block w-52 bg-gray-800'>
+    <div className='min-h-screen h-full sm:hidden md:block fixed top-0  lg:block xl:block w-56 bg-gray-800'>
       <div className="flex flex-row gap-2">
         <img
           src="https://hub.rgukt.ac.in/hub/static/images/logo.png"
-          className="h-9 rounded-[50%]"
+          className="h-9 mt-2 ml-3 rounded-full"
           alt="RGUKT Logo"
         />
-      </div>
-      <h1 className="text-white text-[22px] pl-2 pt-2 font-serif font-bold">
+         <span className="text-white text-[22px] pl-2 pt-2 font-serif font-bold">
         Commities
-      </h1>
+      </span>
+      </div>
+     
       <div className="overflow-y-auto h-full">
-        <ul className="list-none">
-          <Link to="/commtiy1">
-            <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-              Commity 1
+        <ul className="list-none px-2 py-5">
+          <Link to="/academic">
+            <li className="m-2 list-none hover:bg-blue-200  truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+            Academic Committee
             </li>
           </Link>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 2
+          <Link to="/campus">
+            <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+            Campus Amenities Committee
+            </li>
+          </Link>
+          <Link to="/mess">
+            <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+            Mess Advisory Committee
+            </li>
+          </Link>
+          <Link to="/career">
+            <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+            Career Guidance Committee
+            </li>
+          </Link>
+          <Link to="/sports">
+            <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+            Sports Committee
+            </li>
+          </Link>
+          <Link to="/cultural">
+            <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black  text-gray-100 cursor-pointer rounded-md">
+            Cultural Committee
+            </li>
+          </Link>
+          <Link to="/educational">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Educational Opportunities Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 3
+          </Link>
+          <Link to="/external"> 
+            <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+            External Activities Committee
+            </li>
+          </Link>
+          <Link to="/social">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Social Service Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 4
+          </Link>
+         <Link to="/photography">
+         <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Photography Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 5
+         </Link>
+         <Link to="/environment">
+         <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Environment/Sustainability Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 6
+         </Link>
+         <Link to="/campusgreeting">
+         <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Campus Greening Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 7
+         </Link>
+          <Link to="/safety">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Safety and Security Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 8
+          </Link>
+          <Link to="/health">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Health Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 9
+          </Link>
+          <Link to="/placement">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Placement and Internship Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 10
+          </Link>
+          <Link to="/grievance">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Grievance and Redressal Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 11
+          </Link>
+          <Link to="/hostel">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Hostel Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 12
+          </Link>
+          <Link to="/innovation">
+          <li className="m-2 list-none hover:bg-blue-200 truncate hover:text-black text-gray-100 cursor-pointer rounded-md">
+          Innovation and Incubation Committee
           </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 13
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 14
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 15
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 16
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 17
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 18
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 19
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 20
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 21
-          </li>
-          <li className="m-2 list-none text-gray-100 cursor-pointer rounded-md">
-            Commity 22
-          </li>
+          </Link>
         </ul>
       </div>
     </div>
