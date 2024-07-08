a48e9d2b26239230cfd02967e4a707d4075a94ad
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-07-08 11:24:23 +0530

    header added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import HomePage from './components/Home'
+import Homepage from './components/Home'
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<HomePage/>} />
+      <Route exact path="/" Component={Homepage} />
     </Routes>
   </BrowserRouter>
 )
