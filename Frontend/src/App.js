4fa95ad82fd837f8d50ba68a864f2a87fb34f5b8
RathlavathRamesh <rameshrguktbasar@gmail.com>
2024-07-08 11:35:16 +0530

    app modified

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Homepage from './components/Home'
+import Header from './components/Header'
 
 const App = () => (
   <BrowserRouter>
+  <Header/>
     <Routes>
       <Route exact path="/" Component={Homepage} />
     </Routes>
