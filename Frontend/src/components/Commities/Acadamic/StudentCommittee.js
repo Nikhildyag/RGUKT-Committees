c7bda1f66dc39696257b96f723e15c93887e2eb3
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-07-12 16:52:25 +0530

    Updated fonts

--- Frontend/src/components/Commities/Acadamic/StudentCommittee.js
+++ Frontend/src/components/Commities/Acadamic/StudentCommittee.js
@@ -17,9 +17,9 @@ const AcadamicStudentCommittee = () => {
       <AcademicHeader name={'Academic Committee'} />
       <div className="flex w-full">
         <AcademicSidebar />
-        <div className="relative md:left-40  lg:left-60 sm:left-0 z-[-1] flex flex-wrap justify-center items-center mt-10 md:w-[80%] sm:w-[100%]">
-          <div className="mt-8 border border-[#c85cdb] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 md:mx-0">
-            <h1 className=" bg-[#c85cdb]  text-white text-2xl  text-center py-2">
+        <div className="relative md:left-40  lg:left-60 sm:left-0 z-[-1] flex flex-wrap justify-center items-center mt-10 md:w-[80%] sm:w-[100%]  ">
+          <div className="mt-8 border border-[#c85cdb] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 lg:mx-0">
+            <h1 className=" bg-[#c85cdb]  text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl  text-center py-2">
               Computer Science and Engineering
             </h1>
             <div className="flex flex-wrap justify-center items-center  px-4">
@@ -37,8 +37,8 @@ const AcadamicStudentCommittee = () => {
               </div>
             </div>
           </div>
-          <div className="mt-8 border border-[#45d9b9] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 md:mx-0">
-            <h1 className="bg-[#45d9b9]  text-white text-2xl  text-center py-2">
+          <div className="mt-8 border border-[#45d9b9] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 lg:mx-0">
+            <h1 className="bg-[#45d9b9]  text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl  text-center py-2">
               Electronics and Communications Engineering
             </h1>
             <div className="flex flex-wrap justify-center items-center px-4">
@@ -56,8 +56,8 @@ const AcadamicStudentCommittee = () => {
               </div>
             </div>
           </div>
-          <div className="mt-8 border border-[#eb692d] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 md:mx-0">
-            <h1 className="bg-[#eb692d]  text-white text-2xl  text-center py-2">
+          <div className="mt-8 border border-[#eb692d] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 lg:mx-0">
+            <h1 className=" bg-[#eb692d]  text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl  text-center py-2">
               Electronical Engineering
             </h1>
             <div className="flex flex-wrap justify-center items-center px-4">
@@ -75,8 +75,8 @@ const AcadamicStudentCommittee = () => {
               </div>
             </div>
           </div>
-          <div className="mt-8 border border-[#bddc3f] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 md:mx-0">
-            <h1 className="bg-[#bddc3f]  text-white text-2xl  text-center py-2">
+          <div className="mt-8 border border-[#bddc3f] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 lg:mx-0">
+            <h1 className=" bg-[#bddc3f]  text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl  text-center py-2">
               Civil Engineering
             </h1>
             <div className="flex flex-wrap justify-center items-center px-4">
@@ -94,8 +94,8 @@ const AcadamicStudentCommittee = () => {
               </div>
             </div>
           </div>
-          <div className="mt-8 border border-[#e33244] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 md:mx-0">
-            <h1 className="bg-[#e33244]  text-white text-2xl  text-center py-2">
+          <div className="mt-8 border border-[#e33244] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 lg:mx-0">
+            <h1 className=" bg-[#e33244]  text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl  text-center py-2">
               Mechanical Engineering
             </h1>
             <div className="flex flex-wrap justify-center items-center px-4">
@@ -113,8 +113,8 @@ const AcadamicStudentCommittee = () => {
               </div>
             </div>
           </div>
-          <div className="mt-8 border border-[#1d4872] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 md:mx-0">
-            <h1 className="bg-[#1d4872]  text-white text-2xl  text-center py-2">
+          <div className="mt-8 border border-[#1d4872] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 lg:mx-0">
+            <h1 className=" bg-[#1d4872]  text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl  text-center py-2">
               Chemical Engineering
             </h1>
             <div className="flex flex-wrap justify-center items-center px-4">
@@ -132,8 +132,8 @@ const AcadamicStudentCommittee = () => {
               </div>
             </div>
           </div>
-          <div className="mt-8 border border-[#e81791] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 md:mx-0 mb-4">
-            <h1 className="bg-[#e81791]  text-white text-2xl  text-center py-2">
+          <div className="mt-8 border border-[#e81791] rounded-md pb-4 lg:w-[80%] sm:w-[100%] sm:mx-8 lg:mx-0 mb-4">
+            <h1 className=" bg-[#e81791]  text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl  text-center py-2">
               Metallurgical and Material Engineering
             </h1>
             <div className="flex flex-wrap justify-center items-center px-4">
