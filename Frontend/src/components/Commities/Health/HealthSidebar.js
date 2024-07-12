58d2bdc126efcccd78987e253a18c12b6e25da70
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-07-12 14:44:36 +0530

    updated sidebar of 18 committees

--- Frontend/src/components/Commities/Health/HealthSidebar.js
+++ Frontend/src/components/Commities/Health/HealthSidebar.js
@@ -1,14 +1,7 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaAngleDown, FaAngleRight } from 'react-icons/fa'
+
 const HealthSidebar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-
-  const dropdownRef = useRef(null)
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen)
-  }
   return (
     <div className="min-h-screen h-full sm:hidden md:block fixed top-0 lg:block xl:block w-2/12 bg-[#ece8e8] shadow-lg mt-14">
       <div className="overflow-y-auto h-full">
@@ -23,56 +16,11 @@ const HealthSidebar = () => {
               Central Authority
             </li>
           </Link>
-          <li className="py-1  text-black relative" ref={dropdownRef}>
-            <button
-              onClick={toggleDropdown}
-              className="flex items-center justify-between w-full text-left px-2 py-1 focus:outline-none"
-            >
-              <span className="mr-2  text-black ">Committees</span>
-              {isDropdownOpen ? <FaAngleDown /> : <FaAngleRight />}
-            </button>
-            <ul
-              className={`${
-                isDropdownOpen ? 'block' : 'hidden'
-              } ml-2 bg-transparent rounded text-black`}
-            >
-              <li className=" text-black">
-                <Link to="/academic" className="block  text-black ">
-                  CSE
-                </Link>
-              </li>
-              <li className=" text-black">
-                <Link to="/sports" className="block py-1">
-                  ECE
-                </Link>
-              </li>
-              <li className=" text-black">
-                <Link to="/campusamenties" className="block">
-                  EEE
-                </Link>
-              </li>
-              <li className=" text-black">
-                <Link to="/mess" className="block py-1">
-                  CE
-                </Link>
-              </li>
-              <li className=" text-black">
-                <Link to="/hostel" className="block">
-                  ME
-                </Link>
-              </li>
-              <li className=" text-black">
-                <Link to="/career" className="block py-1">
-                  CHE
-                </Link>
-              </li>
-              <li className=" text-black">
-                <Link to="/innovation" className="block">
-                  MME
-                </Link>
-              </li>
-            </ul>
-          </li>
+          <Link to="/health/studentcommitte">
+            <li className="m-2 list-none hover:bg-[#800e08] hover:text-white hover:px-2 truncate text-black cursor-pointer rounded-md py-2">
+              Department Committee
+            </li>
+          </Link>
         </ul>
       </div>
     </div>
