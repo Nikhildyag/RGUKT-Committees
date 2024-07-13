79a40ed2c3836ed5735724f0211dabf79c97d56c
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-07-13 23:32:27 +0530

    Updated Form Card

--- Frontend/src/components/forms/FormCard.js
+++ Frontend/src/components/forms/FormCard.js
@@ -27,19 +27,19 @@ const FormCard = () => {
         <AcademicSidebar />
       </div>
       <div className="lg:mt-24 sm:w-[90%] sm:ml-[5%]  sm:mt-24 sm:px-4 lg:w-[70%] rounded-lg lg:ml-[24%]  md:w-[70%] md:ml-[23%] md:mt-20  flex items-center mb-10  bg-[#0d1d3b] pb-9">
-        <div className="flex flex-col lg:flex-row items-center">
+        <div className="flex flex-col lg:flex-row items-center ">
           <div className="bg-transparent rounded-lg">
             <img
               src="https://cdni.iconscout.com/illustration/premium/thumb/complaint-resolution-specialist-handling-customer-complaints-and-working-to-resolve-them-8978982-7328703.png?f=webp"
               alt="Complaint"
-              className=" lg:w-[80em] lg:h-[30em] md:w-[70em] md:h-[25em] sm:w-[60em] sm:h-[20em] "
+              className=" lg:w-[80em] lg:h-[30em] md:w-[80em] md:h-[25em] sm:w-[60em] sm:h-[20em] "
             />
           </div>
-          <div className="max-w-lg mx-auto sm:mx-4 md:mt-10 sm:mt-2 px-5  bg-[#223b5d] rounded-lg shadow-md  py-6">
+          <div className="max-w-lg lg:w-[100%] mx-auto sm:mx-4 md:mt-0  lg:mt-10 sm:mt-2 px-5  bg-[#223b5d] rounded-lg shadow-md  py-6 ">
             <h1 className=" sm:text-md text-base md:text-lg lg:text-xl font-bold mb-6 text-center text-white">
               Academic Committee Grievance Form
             </h1>
-            <form >
+            <form>
               <div className="mb-4">
                 <label
                   className="block text-white  font-bold mb-2"
