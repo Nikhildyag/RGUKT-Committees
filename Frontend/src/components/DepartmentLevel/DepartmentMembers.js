d91883c610b13bab1fa0b7be629c13d7fddb0c1a
RathlavathRamesh <rameshrguktbasar@gmail.com>
2024-07-14 14:14:40 +0530

    membersapicall made

--- Frontend/src/components/DepartmentLevel/DepartmentMembers.js
+++ Frontend/src/components/DepartmentLevel/DepartmentMembers.js
@@ -1,9 +1,24 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import DepartmentHeader from './DepartmentHeader';
 import DepartmentSidebar from './DepartmentSidebar';
 
 const DepartmentMembers = () => {
+ 
+  const fetchDepartmentMembers =async() => {
+       const url = "http://localhost:1024/api/v1/department/get/committee/departmentMembers";
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.ok === true) {
+      console.log("fetched data is ",data);
+    } else {
+      console.log(response.errorMessage);
+      //console.log(response.ok);
+    }
+  }
+  useEffect(() => {
+    fetchDepartmentMembers();
+  },[])
   return (
     <div className="max-w-[100%] overflow-x-hidden text-wrap">
       <DepartmentHeader />
--- Frontend/src/components/DepartmentLevel/index.js
+++ Frontend/src/components/DepartmentLevel/index.js
@@ -1,46 +1,11 @@
 import DepartmentHeader from './DepartmentHeader'
 import DepartmentSidebar from './DepartmentSidebar'
-import ProfileCard from '../ProfileCard'
 import DepartmentComplaints from './DepartmentComplaints'
-import React, { useEffect } from 'react'
-import Cookies from 'js-cookie'
+import React  from 'react'
 
-const cardData = {
-  name: 'Academic Commity',
-  complaintForm: '/academiccommittefeedbackform',
-  purPose:
-    'The Academic Committee at RGUKT Basar is dedicated to ensuring academic excellence and fostering an environment that promotes intellectual growth and development among students. The committee is committed to continuously improving the academic framework, supporting students in their educational journey, and maintaining high standards of teaching and learning',
-  rolesAndResponsibilities: [
-    'Monitor academic performance and provide support to struggling students.',
-    'Organize academic workshops, seminars, and guest lectures',
-    'Collect feedback on courses and suggest improvements.',
-    'Coordinate with faculty on academic policies and curriculum development.',
-  ],
-}
 
 const Departments = () => {
-  const getDepartmentMembersResult = async () => {
-    const accessToken = Cookies.get('department_user_token')
-    console.log('your cookes is ', accessToken)
-    const url = 'http://localhost:1024/api/v1/department/login'
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: '',
-    })
-    const data = await response.json()
-    if (response.ok === true) {
-      console.log(response.accessToken)
-    } else {
-      console.log(response.errorMessage)
-      console.log(response.ok)
-    }
-  }
-  useEffect(() => {
-    getDepartmentMembersResult()
-  }, [])
+  
   return (
     <div className="max-w-[100%] overflow-x-hidden text-wrap">
       <DepartmentHeader />
