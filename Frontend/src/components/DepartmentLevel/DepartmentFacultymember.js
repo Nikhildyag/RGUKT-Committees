6ca67f8350f314efd56b929abc5f17e5934c05a3
RathlavathRamesh <rameshrguktbasar@gmail.com>
2024-07-14 15:40:18 +0530

    making api calls

--- Frontend/src/components/DepartmentLevel/DepartmentFacultymember.js
+++ Frontend/src/components/DepartmentLevel/DepartmentFacultymember.js
@@ -1,9 +1,38 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DepartmentHeader from './DepartmentHeader';
 import DepartmentSidebar from './DepartmentSidebar';
 
 const DepartmentFacultymember = () => {
+  const [facultyMember, setFacultyMember] = useState();
+
+  const fetchFacultyMember = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:1024/api/v1/incharge/get-incharge",
+          {
+            method: "GET",
+            credentials: "include", // Include credentials (cookies)
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error(response,"Network response was not ok");
+        }
+        const json = await response.json();
+        console.log(json)
+        // set(json.MembersArray);
+        console.log(json);
+      } catch (error) {
+        console.log(error);
+      }
+  }
+  useEffect(() => {
+    fetchFacultyMember();
+  })
+
   return (
     <div className="max-w-[100%] overflow-x-hidden text-wrap">
       <DepartmentHeader />
