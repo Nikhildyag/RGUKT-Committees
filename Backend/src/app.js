f7acf36f07457c6d9d5642d8c334d8a6500cb4d1
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-07-15 10:59:59 +0530

    created routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,20 +1,22 @@
-import express from "express";
-import cors from "cors";
-import InchargeRouter from "./routes/incharge.routes.js";
-import DepartmentRouter from "./routes/departmentMember.routes.js";
-import centralRouter from "./routes/centralMember.routes.js";
-import messagesRouter from "./routes/messages.routes.js";
-import cookieParser from "cookie-parser";
-const app = express();
+import express from 'express'
+import cors from 'cors'
+import InchargeRouter from './routes/incharge.routes.js'
+import DepartmentRouter from './routes/departmentMember.routes.js'
+import centralRouter from './routes/centralMember.routes.js'
+import messagesRouter from './routes/messages.routes.js'
+import cookieParser from 'cookie-parser'
+import complaintRouter from './routes/complaintForm.routes.js'
+const app = express()
 const corsOptions = {
-  origin: ["http://localhost:3000"], // Frontend domain
+  origin: ['http://localhost:3000'], // Frontend domain
   credentials: true, // Allow credentials (cookies) to be sent
-};
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api/v1/incharge", InchargeRouter);
-app.use("/api/v1/department", DepartmentRouter);
-app.use("/api/v1/central", centralRouter);
-app.use("/api/v1/messages", messagesRouter);
-export default app;
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use('/api/v1/incharge', InchargeRouter)
+app.use('/api/v1/department', DepartmentRouter)
+app.use('/api/v1/central', centralRouter)
+app.use('/api/v1/messages', messagesRouter)
+app.use('/api/v1/complaints', complaintRouter)
+export default app
---
+++ Backend/src/routes/complaintForm.routes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express'
+import { Feedback } from '../models/feedback.model.js'
+
+const router = Router()
+export default router
