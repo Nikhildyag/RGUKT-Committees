a5e398e02a78252bdcd57b53022d7de442ca937d
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-07-16 17:11:50 +0530

    Styled Complaint card

--- Frontend/src/components/DepartmentLevel/ViewComplaint.js
+++ Frontend/src/components/DepartmentLevel/ViewComplaint.js
@@ -3,11 +3,11 @@ import { useParams } from 'react-router-dom'
 
 const ViewComplaint = () => {
   const { complaintId } = useParams()
-  const [remarks, setRemarks] = useState();
+  const [remarks, setRemarks] = useState()
   const [complaint, setComplaint] = useState(null)
-  const [status, setStatus] = useState('pending');
+  const [status, setStatus] = useState('pending')
   useEffect(() => {
-    const fetchComplaint =async() => {  
+    const fetchComplaint = async () => {
       try {
         const response = await fetch(
           `http://localhost:1024/api/v1/complaints/get/particularComplaint/${complaintId}`,
@@ -24,7 +24,7 @@ const ViewComplaint = () => {
         }
         const json = await response.json()
         setComplaint(json.complaint)
-        setStatus(json.complaint.status);
+        setStatus(json.complaint.status)
       } catch (error) {
         console.log(error)
       }
@@ -33,31 +33,30 @@ const ViewComplaint = () => {
   }, [complaintId])
 
   const formatDate = (dateString) => {
-    const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
-};
-
+    const date = new Date(dateString)
+    const day = String(date.getDate()).padStart(2, '0')
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const year = date.getFullYear()
+    return `${day}/${month}/${year}`
+  }
 
   const handleStatusChange = (e) => {
     const newStatus = e.target.value
-    setStatus(newStatus);
+    setStatus(newStatus)
   }
   const submitComplaint = async (e) => {
-     e.preventDefault();
+    e.preventDefault()
     if (!status || !remarks) {
-      alert("all feilds required");
-      return;
-    } 
+      alert('all feilds required')
+      return
+    }
     const data = {
       status,
-      centralRemarks:remarks,
-      complaint_id:complaintId
+      centralRemarks: remarks,
+      complaint_id: complaintId,
     }
-    setRemarks('');
-    const complaintDetails = JSON.stringify(data);
+    setRemarks('')
+    const complaintDetails = JSON.stringify(data)
     try {
       const response = await fetch(
         `http://localhost:1024/api/v1/complaints/update-complaint`,
@@ -67,19 +66,19 @@ const ViewComplaint = () => {
           headers: {
             'Content-Type': 'application/json',
           },
-          body:complaintDetails
+          body: complaintDetails,
         }
       )
       if (!response.ok) {
         throw new Error('Network response was not ok')
-        }
-      const json = await response.json();
-      alert("status ubdated");
+      }
+      const json = await response.json()
+      alert('status ubdated')
       console.log(json)
     } catch (error) {
-      alert("Error in ubdating the status")
-        console.log(error)
-      }
+      alert('Error in ubdating the status')
+      console.log(error)
+    }
   }
 
   if (!complaint) {
@@ -90,53 +89,60 @@ const ViewComplaint = () => {
       <h2 className="text-3xl font-bold mb-4 text-gray-800 text-center">
         Complaint Details
       </h2>
-      <div className="bg-white rounded-lg shadow-lg overflow-hidden sm:rounded-lg border border-gray-300 hover:border-blue-500 transition duration-300">
+      <div className="bg-[#0d1d3b] rounded-lg shadow-lg overflow-hidden sm:rounded-lg border border-gray-300 hover:border-blue-500 transition duration-300">
         <div className="p-4">
-          <div className="flex flex-col border-b border-gray-300 hover:bg-gray-100">
+          <div className="flex flex-col border-b border-gray-300  px-4">
             <div className="py-2 border-b border-gray-300">
-              <p className="text-sm text-gray-600 mb-2">
-                <span className="font-medium">ID:</span> {complaint._id}
+              <p className="text-sm md:text-md text-white mb-2">
+                <span className="md:text-md">ID:</span> {complaint._id}
               </p>
-              <p className="text-sm text-gray-600 mb-2">
-                <span className="font-medium">Category:</span>{' '}
+              <p className="text-sm md:text-md text-white mb-2">
+                <span className="md:text-md">Category:</span>{' '}
                 {complaint.category}
               </p>
-              <p className="text-sm text-gray-600 mb-2">
-                <span className="font-medium">Date:</span> {formatDate(complaint.createdAt)}
+              <p className="text-sm md:text-md text-white mb-2">
+                <span className="md:text-md font-medium">Date:</span>{' '}
+                {formatDate(complaint.createdAt)}
               </p>
-              <div className="text-sm text-gray-600 mb-2">
-                <span className="font-medium">Status:</span>{' '}
+              <div className="text-sm md:text-md text-white mb-2">
+                <span className="md:text-md">Status:{` `}</span>
                 <select
-                  className="border border-gray-300 rounded px-2 py-1 mt-1"
+                  className="border border-gray-300 rounded px-2 py-1 mt-1 ml-0 md:text-md text-black"
                   value={status}
                   onChange={handleStatusChange}
                 >
                   <option value="pending">Pending</option>
                   <option value="resolved">Resolved</option>
-                  <option value="forwarded">
-                    Raised to Central Authority
-                  </option>
+                  <option value="forwarded">Raised to Central Authority</option>
                 </select>
               </div>
             </div>
             <div className="py-2">
-              <p className="text-sm text-gray-700">
-                <span className="font-medium">Description:</span>{' '}
+              <p className="text-sm md:text-md text-white">
+                <span className="md:text-md">Description:</span>{' '}
                 {complaint.description}
               </p>
             </div>
-            <div className="text-sm flex flex-col mb-4">
-              <label>Remarks:</label>
+            <div className="text-sm flex flex-col mb-4 md:text-md">
+              <label className="text-white">Remarks:</label>
               <textarea
-                className="w-auto px-2 py-2 my-2 border border-blue-500"
-                onChange={(e)=>setRemarks(e.target.value)}
+                className="w-auto px-2 py-2 my-2  text-black"
+                onChange={(e) => setRemarks(e.target.value)}
                 placeholder="Remarks to be raised to central authority"
                 value={remarks}
               ></textarea>
             </div>
+            <div className="mb-4">
+              <button
+                type="submit"
+                onClick={submitComplaint}
+                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md md:text-md"
+              >
+                Submit Complaint
+              </button>
+            </div>
           </div>
         </div>
-        <button type='submit' onClick={submitComplaint} className='bg-gray-200'>submit complaint</button>
       </div>
     </div>
   )
