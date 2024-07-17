b0a1d5df0108b305b12c3eebef5c65305146e25a
Nikhil Reddy <nikhildyaga123@gmail.com>
2024-07-17 11:39:08 +0530

    temp

--- Backend/src/routes/notice.routes.js
+++ Backend/src/routes/notice.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createNotice, showNotices } from "../controllers/notice.controller";
+import { verifyCentralJWT } from "../middlewares/auth.middleware";
 const router = Router();
-router.route("/create-notice").post(createNotice);
+router.route("/create-notice").post(verifyCentralJWT, createNotice);
 router.route("get/notices").get(showNotices);
 export default router;
