9aa8e4173f844bc848431c013d860b9327d7b7f3
Sathwika Vontela <sathwikavontela123@gmail.com>
2024-08-03 13:06:42 +0530

    updated cards

--- Frontend/src/components/ProfileCard/index.js
+++ Frontend/src/components/ProfileCard/index.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
 const ProfileCard = ({ Data }) => {
-  console.log("profile card", Data);
+  console.log('profile card', Data)
   return (
     <div className="flex justify-center mt-8 md:mt-10">
       <div className="max-w-xs md:max-w-lg">
-        <div className="flex flex-row items-center lg:w-[25em] sm:w-fit sm:px-2 md:w-fit md:px-4 bg-white border border-gray-200 rounded-lg shadow-md">
+        <div className="flex flex-row items-center lg:w-[23em] sm:w-[18em] sm:px-2 md:w-fit md:px-4 bg-white border border-gray-200 rounded-lg shadow-md">
           <img
-            className="object-cover  rounded-t-lg h-auto w-32 md:rounded-none md:rounded-l-lg"
+            className="object-cover  rounded-t-lg md:h-32 md:w-32 sm:w-32 sm:h-[8em] md:rounded-none md:rounded-l-lg"
             src={Data.image_url}
             alt="Profile"
           />
@@ -25,7 +25,7 @@ const ProfileCard = ({ Data }) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProfileCard;
+export default ProfileCard
