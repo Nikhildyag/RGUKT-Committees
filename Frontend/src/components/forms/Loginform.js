5db144f964c093049a78ed0536cb771b3a8e63b1
RathlavathRamesh <rameshrguktbasar@gmail.com>
2024-08-14 10:58:44 +0530

    spinner is added for few pages

--- Frontend/src/components/forms/Loginform.js
+++ Frontend/src/components/forms/Loginform.js
@@ -12,6 +12,7 @@ const Loginform = () => {
   const inchargeInfo = Cookies.get("Faculty_jwt_token");
   const centralInfo = Cookies.get("Central_jwt_token");
   const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     if (departmentInfo) {
       navigate("/departments");
@@ -28,6 +29,7 @@ const Loginform = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     if (!username.current.value || !password.current.value) {
       toast.error("All fields are required");
     }
@@ -53,6 +55,7 @@ const Loginform = () => {
       }
 
       const data = await response.json();
+      setIsLoading(false);
       const member = data.member;
       if (member.username.startsWith("central")) {
         localStorage.setItem("central", JSON.stringify(member));
@@ -88,17 +91,17 @@ const Loginform = () => {
     } catch (error) {
       console.error("Login error:", error);
       setErrorMessage(true);
+      setIsLoading(false);
     }
   };
-
+  console.log(isLoading);
   return (
     <div className="relative">
       <ToastContainer />
       <div className="opacity-25">
         <Homepage />
       </div>
-
-      <div className="absolute top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center bg-gray-950 bg-opacity-75 h-screen">
+     {!isLoading && <div className="absolute top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center bg-gray-950 bg-opacity-75 h-screen">
         <div className="relative w-full max-w-xs md:max-w-2xl px-4 py-2 lg:max-w-3xl bg-white rounded-lg shadow-md flex flex-col md:flex-row items-center sm:mb-[5%] md:mt-[6%]">
           {/* Close Button */}
           <button
@@ -172,7 +175,15 @@ const Loginform = () => {
             </form>
           </div>
         </div>
+      </div>}
+      {isLoading && <div role="status">
+        <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
       </div>
+      }
     </div>
   );
 };
